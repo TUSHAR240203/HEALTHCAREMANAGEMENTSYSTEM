3f9ffc1509f3a7b44a0fd494ed42fca02a3dbe63
Tushar Sharma <tushar.6036@gmail.com>
2026-04-27 15:44:12 +0530

    Done

--- Hms.PatientsApi/Program.cs
+++ Hms.PatientsApi/Program.cs
@@ -5,8 +5,22 @@ using Hms.PatientsApi.Middleware;
 using Hms.PatientsApi.Repositories;
 using Hms.PatientsApi.Services;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File(
+        "Logs/patients-api-log-.txt",
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 7
+    )
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +34,8 @@ builder.Services.AddScoped<IPatientService, PatientService>();
 
 var app = builder.Build();
 
+app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,7 +43,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.UseMiddleware<ExceptionMiddleware>();
+
 app.Run();
