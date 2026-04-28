89890d4ebf5dff4158e596af9410ab8089e6cdee
Kritika Sharma <kritikasharma0514@gmail.com>
2026-04-28 10:21:53 +0530

    doctor api tests

---
+++ Hms.PatientsApi/Migrations/20260425043503_new1init.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hms.PatientsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class new1init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
