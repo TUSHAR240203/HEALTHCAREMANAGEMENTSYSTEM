8cef7885c0d0723c3dbcdaf3c8749a6c6f886646
Kritika Sharma <kritikasharma0514@gmail.com>
2026-05-01 15:36:41 +0530

    doctor profile

--- Hms.DoctorsApi.Tests/TestHelpers/TestData.cs
+++ Hms.DoctorsApi.Tests/TestHelpers/TestData.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Hms.DoctorsApi.DTOs.Doctors;
 using Hms.DoctorsApi.Entities;
 using Hms.DoctorsApi.Mapping;
+using Microsoft.Extensions.Logging;
 
 namespace Hms.DoctorsApi.Tests.TestHelpers;
 
@@ -9,11 +10,16 @@ public static class TestData
 {
     public static IMapper CreateMapper()
     {
-        var config = new MapperConfiguration(cfg => cfg.AddProfile<DoctorMappingProfile>());
+        using var loggerFactory = LoggerFactory.Create(builder => { });
+
+        var config = new MapperConfiguration(
+            cfg => cfg.AddProfile<DoctorMappingProfile>(),
+            loggerFactory);
+
         config.AssertConfigurationIsValid();
+
         return config.CreateMapper();
     }
-
     public static CreateDoctorRequestDto CreateDoctorRequest() => new()
     {
         FullName = "Dr John Smith",
